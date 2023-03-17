9345715d74c9671338912ba56058054590218306
Jihye <kimji9663@nate.com>
2023-03-17 19:10:11 +0900

    add

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,7 @@
 import * as React from "react"
+import { Global } from "@emotion/react"
+
+
 
 const Footer = () => {
 
--- src/pages/room.js
+++ src/pages/room.js
@@ -1,15 +1,87 @@
-import React from "react"
+import React, { useEffect, useState, useRef } from "react"
 import { Link, graphql } from "gatsby" 
 import Layout from "../components/layout"
 import Bio from "../components/bio"
 import Seo from "../components/seo"
-import Header from "../components/header"
+import { Container } from "./room.styles"
+import styled from "@emotion/styled"
+import { Global, css } from "@emotion/react"
+import useElementPosition from '../components/useElementPosition'
+import { HeaddingText, HeaddingWrap } from "./room.styles"
+  
 
-const Room = ({ data, location }) => {
-    const siteTitle = data.site.siteMetadata?.title || `Title`
+//const ConButton = Container.withComponent("button");
+
+const UserButton = props => (
+  <div>
+    button!!!
+    <span>0</span>
+    <span>0</span>
+    <span>0</span>
+  </div>
+)
+
+const globalStyle = css({
+  'body': {
+    margin: 0,
+    padding: 0,
+    background: '#ecebeb',
+    color: '#777',
+  },
+})
+
+const headingData = [
+  {
+    headding: `section01...!`
+  },
+  {
+    headding: `section02...!`
+  },
+  {
+    headding: `section03...!`
+  },
+  {
+    headding: `section04...!`
+  },
+]
+
+const Room = ({ data, location }, props) => {
+    //const siteTitle = data.site.siteMetadata?.title || `Title`
+    const [textIndex, setTextIndex] = useState(0)
+    const elementRef = useRef(null)
+    const totalFrames = 144
+    const imageFrame = useElementPosition(elementRef, totalFrames, 1)
+
+    useEffect(() => {
+      let index = getTextIndexPercentage()
+      if (index < headingData.length) setTextIndex(index)
+      console.log(imageFrame)
+    }, [imageFrame])
+
+    function getTextIndexPercentage() {
+      return Math.floor((headingData.length * imageFrame) / totalFrames )
+    }
 
     return (
-      <p>123</p>
+      <>
+        <Global styles={globalStyle} />
+        <div ref={elementRef}>
+          <div imageframe={imageFrame}>
+            <HeaddingWrap index={textIndex} datalength={headingData.length}>
+              {headingData?.map(({headding}, i) => {
+                return (
+                  <HeaddingText className={`section_${i}`} key={i} first={i === 0 ? true : false}>
+                    <h1 style={{
+                      opacity: i === textIndex ? 1 : 0
+                    }}>{headding}</h1>
+                  </HeaddingText>
+                )
+              })}
+            </HeaddingWrap>
+          </div>
+        </div>
+        <UserButton />
+      </>
     )
 }
 
