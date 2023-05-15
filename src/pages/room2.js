545491e4ae968c5041d90e492e7aa26f98b25cbe
Jihye <kimji9663@nate.com>
2023-05-15 18:43:45 +0900

    room2 test

--- src/pages/room2.js
+++ src/pages/room2.js
@@ -4,12 +4,8 @@ import Seo from "../components/seo"
 import { Global, css } from "@emotion/react"
 import useElementPosition from '../components/useElementPosition'
 
-// img
-import video_room01 from '../images/video_room01.mp4'
-//import video_room01 from '../images/sample-low.mp4'
-
 // css
-import { Main, MainInner, VideoHeading, HeaddingText, HeaddingWrap, Video } from "../style/room.styles"
+import { Main, MainInner, VideoHeading, HeaddingText, HeaddingWrap } from "../style/room2.styles"
 
 //const ConButton = Container.withComponent("button")
 
@@ -28,7 +24,7 @@ const globalStyle = css({
 
 const headingData = [
   {
-    headding: `Let me introduce My room.`
+    headding: `1989.11.25`
   },
   {
     headding: `This is my desktop.`
@@ -45,17 +41,10 @@ const Room2 = () => {
     const [textIndex, setTextIndex] = useState(0)
     const elementRef = useRef(null)
     //const videoRef = useRef(null)
-    const totalFrames = 200
+    const totalFrames = 410
     const videoFrame = Math.floor(useElementPosition(elementRef, totalFrames, 0))
-    //const getImage = (frame) => (`https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${frame.toString().padStart(4, '0')}.jpg`)
     const getImage = (frame) => (`https://raw.githubusercontent.com/kimji9663/kimji_blog/main/src/images/cloud/hero-desktop.8e197ed_out${frame.toString().padStart(4, '0')}.jpg`)
     let scrollPosition = 0
-
-    // const videoScroll = (datalength) => {
-    //   scrollPosition = videoFrame / datalength
-    //   videoRef.current.currentTime = scrollPosition
-    //   //console.log(videoRef.current.currentTime, scrollPosition)
-    // }
 
     const scrollTop = () => {
       window.scrollTo({
@@ -64,20 +53,6 @@ const Room2 = () => {
       })
     }
 
-    // function videoToBlob(localImage) {
-    //   let reader = new  FileReader()
-    //   reader.readAsArrayBuffer(localImage)
-
-    //   let buffer = reader.result
-    //   let videoBlob = new Blob([new Uint8Array(buffer)], { type: 'video/mp4' })
-    //   let url = window.URL.createObjectURL(videoBlob)
-    //   console.log(url)
-    // }
-
-    // useEffect(() => {
-    //   videoToBlob(video_room01)
-    // }, [])
-
     // set loading
     const [loading, setLoading] = useState(true)
     const loadingTime = 3000
@@ -85,7 +60,6 @@ const Room2 = () => {
     useEffect(() => {
       let textPercentage = Math.floor((headingData.length * videoFrame) / totalFrames * 10 ) // 0 ~ 4
       textPercentage < headingData.length ? setTextIndex(textPercentage) : setTextIndex(3)
-      //videoScroll(headingData.length) // video scroll
       console.log('###', videoFrame, totalFrames, textPercentage)
     }, [videoFrame])
 
@@ -107,6 +81,7 @@ const Room2 = () => {
         <Main ref={elementRef}>
           <MainInner videoFrame={videoFrame}>
             <VideoHeading index={textIndex}>
+              <img src={getImage(videoFrame)} alt=""/>
               <HeaddingWrap index={textIndex} datalength={headingData.length}>
                 <div>
                   {headingData?.map(({headding}, i) => {
@@ -118,12 +93,6 @@ const Room2 = () => {
                   })}
                 </div>
               </HeaddingWrap>
-              <img src={getImage(videoFrame)} alt=""/>
-              {/* <Video index={textIndex}>
-                <video ref={videoRef} autobuffer="autobuffer" preload="preload">
-                  <source type="video/mp4; codecs=&quot;avc1.42E01E, mp4a.40.2&quot;" src={video_room01}></source>
-                </video>
-              </Video> */}
             </VideoHeading>
           </MainInner>
         </Main>
---
+++ src/style/room2.styles.js
@@ -0,0 +1,157 @@
+import styled from "@emotion/styled"
+import { keyframes } from "@emotion/react"
+import { mq } from '../constants/index'
+
+export const Main = styled.div(() => ({
+    position: 'relative',
+    width: '100%',
+    height: '800vh',
+}));
+
+export const MainInner = styled.div(() => ({
+    position: 'sticky',
+    top: 0,
+    width: '100%',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+}));
+
+export const BackgroundImg1 = styled.div(({ total, index }) => ({
+    position: 'absolute',
+    top: index <= total/1.19 ? `calc(${index}% - 150px)` : `calc(${total/1.19}% - 150px)`,
+    left: index <= total/2 ? `calc(${index}% - 150px)` : (index <= total/1.19 ? `calc(${total - index}% - 150px)` : `calc(${total - total/1.19}% - 150px)`),
+    zIndex: 1,
+    width: '200px',
+    height: '300px',
+    borderRadius: '100px',
+    backgroundColor: 'rgb(255 212 189 / 40%)',
+    transform: `translate3d(0, 0, 0) rotateZ(${index * 10}deg) scale3d(1, 1, 1)`,
+    animation: `${slime} 3s linear infinite`,
+    transition: 'all 01s ease-in-out',
+}))
+
+export const BackgroundImg2 = styled.div(({ total, index }) => ({
+    position: 'absolute',
+    top: index <= total/2 ? `calc(${index}% - 30px)` : `calc(${total - index}% - 30px)`,
+    left: index <= total/1.19 ? `calc(${index}% + 120px)` : `calc(${total/1.19}% + 120px)`,
+    zIndex: 1,
+    width: '200px',
+    height: '170px',
+    borderRadius: '90px',
+    backgroundColor: 'rgb(255 214 227 / 50%)',
+    transform: `translate3d(0, 0, 0) rotateZ(${index * 5}deg) scale(${1 + index/100})`,
+    animation: `${slime} 3s linear infinite`,
+    transition: 'all 01s ease-in-out',
+}))
+
+export const BackgroundImg3 = styled.div(({ total, index }) => ({
+    position: 'absolute',
+    top: index <= total/2 ? `calc(90% - ${index}%)` : `calc(90% - ${total - index}%)`,
+    right: `calc(${index}%)`,
+    zIndex: 1,
+    width: '500px',
+    height: '500px',
+    borderRadius: '250px',
+    backgroundColor: 'rgb(245 213 255 / 40%)',
+    transform: `translate3d(0, 0, 0) rotateZ(-${index * 5}deg) scale(1)`,
+    animation: `${slime} 3s linear infinite`,
+    transition: 'all 01s ease-in-out',
+}))
+
+export const VideoHeading = styled.div(({ index }) => mq({
+    display: 'flex',
+    background: [index === 0 ? 'linear-gradient(to bottom, #362c1b, #000 30%, #000)' : (index === 1 ? 'linear-gradient(to bottom, #313939, #000 30%, #000)' : (index === 2 ? 'linear-gradient(to bottom, #48321f, #000 30%, #000)' : 'linear-gradient(to bottom, #063224, #000 30%, #000)')),
+    index === 0 ? 'linear-gradient(to bottom, #362c1b, #000 30%, #000)' : (index === 1 ? 'linear-gradient(to bottom, #313939, #000 30%, #000)' : (index === 2 ? 'linear-gradient(to bottom, #48321f, #000 30%, #000)' : 'linear-gradient(to bottom, #063224, #000 30%, #000)')),
+    index === 0 ? 'linear-gradient(to right, #362c1b, #000 50%, #000)' : (index === 1 ? 'linear-gradient(to right, #313939, #000 50%, #000)' : (index === 2 ? 'linear-gradient(to right, #48321f, #000 50%, #000)' : 'linear-gradient(to right, #063224, #000 50%, #000)'))],
+    width: '100%',
+    height: '100%',
+    '& > img': {
+        width: '100%',
+    },
+}));
+
+export const HeaddingWrap = styled.div(({ index, datalength }) => mq({
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    height: '100%',
+    //overflow: 'hidden',
+    '& > div': {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        position: 'absolute',
+        top: `-${100 * index}%`,
+        left: 0,
+        zIndex: 1,
+        width: '100%',
+        height: `${100 * datalength}%`,
+    }
+}));
+
+export const HeaddingText = styled.div(({ section, index }) => mq({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    boxShadow: ['0px 14px 20px #00000080', '0px 14px 20px #00000080', '10px 0px 20px #00000080'],
+    opacity: section === index ? 1 : 0,
+
+    '& > h1': {
+        flex: '1 1 auto',
+        order: index % 2 === 1 ? 2 : 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative',
+        fontSize: ['40px', '60px', '80px'],
+        textAlign: 'center',
+        opacity: section === index ? 1 : 0,
+        top: section === index ? '-50px' : '50px',
+        transition: 'opacity, top 1s ease',
+        animation: index === 3 ? `${flicker} 12s linear infinite` : 'none',
+        //color: ['green', 'gray', '#64f0aa'],
+        color: '#fff',
+        textFillColor: 'transparent',
+        background: index === 0 ? 'url(https://thumbs.gfycat.com/CandidHappyBlackbuck-size_restricted.gif) repeat' : (index === 1 ? '#dddddd' : (index === 2 ? '#ffbc85' : '#64f0aa')),
+        backgroundClip: 'text',
+        backgroundPosition: '50% 73%',
+    },
+    '& > div': {
+        flex: '1 1 auto',
+        order: index % 2 === 1 ? 1 : 2,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '50%',
+        backgroundColor: '#fff'
+    }
+}));
+
+/*** ANIMATION ***/
+export const slime = keyframes`
+    0% {border-radius: 36% 64% 63% 37%/54% 60% 44% 48%}
+    25% {border-radius: 73% 35% 55% 60%/65% 44% 62% 56%}
+    50% {border-radius: 75% 35% 35% 72%/58% 51% 56% 49%}
+    75% {border-radius: 71% 52% 69% 31%/69% 52% 79% 58%}
+    100% {border-radius: 36% 64% 63% 37%/54% 60% 44% 48%}
+`
+
+
+export const flicker = keyframes`
+    0% {opacity: 0.9}
+    9% {opacity: 0.3}
+    12% {opacity: 1}
+    20% {opacity: 0.5}
+    25% {opacity: 0.3}
+    30% {opacity: 1}
+    66% {opacity: 1}
+    70% {opacity: 0.7}
+    72% {opacity: 0.4}
+    77% {opacity: 0.9}
+    100% {opacity: 0.9}
+`
